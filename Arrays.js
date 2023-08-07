bffd5d4aa4e6441ca6abf7ee8e868c08d3cedbf1
AashishPanthee <110230844+AashishPanthee@users.noreply.github.com>
2023-08-07 18:09:15 +0545

    another way of declaring array type

--- Arrays.js
+++ Arrays.js
@@ -1,24 +1,17 @@
-const array1:[]=[];
-const array2:string[]=[];
+var array1 = [];
+var array2 = [];
 array2.push("Hello");
 array2.push("World");
 //  array2.push(4);  Assignment of number type is not allowed in string array
 console.log(array2);
- 
-const array3:number[]=[2,3];
+var array3 = [2, 3];
 //  array3.push("hello"); Assignment of string type is not allowed in number array
 console.log(array3);
-
-const array4:Array<boolean>=[];
+var array4 = [];
 array4.push(true);
 console.log(array4);
-
-type Point={
-    x:number;
-    y:number;
-}
-const array5:Point[]=[];
+var array5 = [];
 // array5.push("Aashish"); // Error: Argument of type 'string' is not assignable to parameter of type 'Point'
-array5.push({x:4,y:5});
+array5.push({ x: 4, y: 5 });
 //  array5.push({x:4});  Error: property y is missing
 console.log(array5);

a9f8d4c1609d2c05fbc57af9256958af4590001a
AashishPanthee <110230844+AashishPanthee@users.noreply.github.com>
2023-08-07 22:25:28 +0545

    Type Narrowing with Union Types

--- Union.js
+++ Union.js
@@ -12,3 +12,25 @@ var coordinates = {
     x: 1, y: 34
 };
 coordinates = { lat: 321.33, long: 442.44 };
+// Union Type With Functions
+function printAge(age) {
+    console.log("Age is ".concat(age));
+}
+;
+printAge(23);
+printAge("twenty three");
+// Unions- Narrowing the Type 
+/* Narrowing the Type is simply doing a type check before working with a value.
+If your value is a string you may want to use it differently than if you got a number.
+Since unions allow multiple types for a value, it's good to check what came
+through before working with it. */
+function calculateTax(price, tax) {
+    // return price*tax;   here , price could be number or string, if it is string, then it will give error.
+    // so Type Narrowing is needed here,
+    if (typeof price === "string") {
+        price = parseFloat(price.replace("$", ""));
+    }
+    return price * tax;
+}
+console.log(calculateTax(45, 0.5));
+console.log(calculateTax("$45", 0.5));
--- Union.ts
+++ Union.ts
@@ -24,3 +24,30 @@ let coordinates:Point|Loc={
     x:1,y:34
 }
 coordinates={lat:321.33,long:442.44 }
+
+// Union Type With Functions
+
+function printAge(age:number|string):void{
+    console.log(`Age is ${age}`);
+};
+printAge(23);
+printAge("twenty three");
+
+// Unions- Narrowing the Type 
+/* Narrowing the Type is simply doing a type check before working with a value.
+If your value is a string you may want to use it differently than if you got a number.
+Since unions allow multiple types for a value, it's good to check what came 
+through before working with it. */ 
+
+function calculateTax(price:number|string,tax:number):number{
+    // return price*tax;   here , price could be number or string, if it is string, then it will give error.
+    // so Type Narrowing is needed here,
+    if(typeof price==="string"){
+        price=parseFloat(price.replace("$",""));
+    }
+    return price*tax;
+}
+
+console.log(calculateTax(45,0.5));
+console.log(calculateTax("$45",0.5));
+

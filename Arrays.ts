0b5f258df758408c6bd578477ca3217ae3acf248
AashishPanthee <110230844+AashishPanthee@users.noreply.github.com>
2023-08-07 17:49:49 +0545

    working with array types

---
+++ Arrays.ts
@@ -0,0 +1,11 @@
+const array1:[]=[];
+const array2:string[]=[];
+array2.push("Hello");
+array2.push("World");
+//  array2.push(4);  Assignment of number type is not allowed in string array
+console.log(array2);
+ 
+const array3:number[]=[2,3];
+//  array3.push("hello"); Assignment of string type is not allowed in number array
+console.log(array3);
+

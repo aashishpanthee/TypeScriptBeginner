ac893e407e502f5b51ebfcea0313d4f023195fbc
AashishPanthee <110230844+AashishPanthee@users.noreply.github.com>
2023-08-07 12:37:16 +0545

    creating type alias for objects

--- Objects.js
+++ Objects.js
@@ -1,5 +1,19 @@
+// Object type annotation
+var coordinate = { x: 20, y: 50 };
 //  A function with an object type parameter
 var object1 = function (person) {
     console.log("".concat(person.first, " ").concat(person.last));
 };
 object1({ first: "Aashish", last: "Panthee" });
+var person1 = { firstName: "Aashish", lastName: "Panthee", grade: 12 };
+console.log(person1);
+// Type alias with function parameter
+var printPerson = function (person) {
+    console.log("".concat(person.firstName, " ").concat(person.lastName, " reads in class ").concat(person.grade));
+};
+printPerson({ firstName: "Aashish", lastName: "Panthee", grade: 12 });
+function doublePoint(point) {
+    return { x: point.x * 2, y: point.y * 2 };
+}
+var poi = doublePoint({ x: 10, y: 10 });
+console.log(poi);
--- Objects.ts
+++ Objects.ts
@@ -12,3 +12,38 @@ object1({first:"Aashish",last:"Panthee"});
 //     return true;     /* so if we return any other thing than object , then it will throw error */
 //     return {x:Math.random(),y:Math.random()};  /* this is correct */
 // }
+
+
+
+// Type Alias
+/* Instead of writing out object types in an annotation, we can declare them separately in a type alias,
+ which is simply desired shape of the obect */
+
+// type alias
+type Person={
+    firstName:string;
+    lastName:string;
+    grade:number;
+}
+
+type Point={
+    x:number;
+    y:number;
+}
+
+let person1:Person={firstName:"Aashish",lastName:"Panthee",grade:12};
+console.log(person1);
+
+
+// Type alias with function parameter
+const printPerson=(person:Person):void=>{
+    console.log(`${person.firstName} ${person.lastName} reads in class ${person.grade}`)
+}
+printPerson({firstName:"Aashish",lastName:"Panthee",grade:12});
+
+
+function doublePoint(point:Point):Point{
+    return {x:point.x*2,y:point.y*2}
+}
+let poi=doublePoint({x:10,y:10});
+console.log(poi);

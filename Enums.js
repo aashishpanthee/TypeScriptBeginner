f68d524826ec7d1be5df20033854c6cdb709826b
AashishPanthee <110230844+AashishPanthee@users.noreply.github.com>
2023-08-09 09:41:35 +0545

    Introduction to Enums

---
+++ Enums.js
@@ -0,0 +1,22 @@
+/* Enums allows us to define a set of named constants.
+We can give these constants numeric or string values.
+There's quite a few options when it comes to enums! */
+// Numeric enums
+var Responses;
+(function (Responses) {
+    Responses[Responses["no"] = 0] = "no";
+    Responses[Responses["yes"] = 1] = "yes";
+    Responses[Responses["maybe"] = 2] = "maybe";
+})(Responses || (Responses = {}));
+var OrderStatus;
+(function (OrderStatus) {
+    OrderStatus[OrderStatus["PENDING"] = 0] = "PENDING";
+    OrderStatus[OrderStatus["SHIPPED"] = 1] = "SHIPPED";
+    OrderStatus[OrderStatus["DELIVERED"] = 2] = "DELIVERED";
+    OrderStatus[OrderStatus["RETURNED"] = 3] = "RETURNED";
+})(OrderStatus || (OrderStatus = {}));
+var myStatus = OrderStatus.DELIVERED;
+function isDelivered(status) {
+    return status === OrderStatus.DELIVERED;
+}
+console.log(isDelivered(myStatus));
---
+++ Enums.ts
@@ -0,0 +1,23 @@
+/* Enums allows us to define a set of named constants.
+We can give these constants numeric or string values.
+There's quite a few options when it comes to enums! */
+
+// Numeric enums
+enum Responses{
+    no,
+    yes,
+    maybe,
+}
+
+enum OrderStatus{
+    PENDING,
+    SHIPPED,
+    DELIVERED,
+    RETURNED,
+}
+const myStatus=OrderStatus.DELIVERED;
+
+function isDelivered(status:OrderStatus){
+    return status===OrderStatus.DELIVERED;
+}
+console.log(isDelivered(myStatus));

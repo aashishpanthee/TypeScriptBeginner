03f1df50dc9d547ebc7345e07d6844e090b72551
Aashish Panthee <aashishpanthee060@gmail.com>
2023-08-12 13:21:58 +0545

    Create a Todo List
    Todolist is created by using DOM Manipulation

--- miniproject/dist/index.js
+++ miniproject/dist/index.js
@@ -2,13 +2,16 @@
 const btn = document.getElementById('btn');
 const input = document.getElementById('todoinput');
 const form = document.querySelector('#todoform');
+const todolist = document.querySelector('#todolist');
 function handleSubmit(e) {
     e.preventDefault();
-    console.log('Submitted');
+    const todotext = input.value;
+    const newTodo = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    newTodo.append(todotext);
+    newTodo.append(checkbox);
+    todolist.append(newTodo);
+    input.value = "";
 }
 form.addEventListener('submit', handleSubmit);
-// --------------OR-----------------
-/* form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    console.log('Submitted');
-}) */
--- miniproject/src/index.ts
+++ miniproject/src/index.ts
@@ -1,14 +1,17 @@
 const btn=document.getElementById('btn') as HTMLButtonElement;
 const input=document.getElementById('todoinput') as HTMLInputElement;
 const form=document.querySelector('#todoform') as HTMLFormElement
+const todolist=document.querySelector('#todolist')!;
 function handleSubmit(e:SubmitEvent){
 e.preventDefault();
-console.log('Submitted');
-
+const todotext=input.value;
+const newTodo=document.createElement('li');
+const checkbox=document.createElement('input');
+checkbox.type='checkbox';
+newTodo.append(todotext);
+newTodo.append(checkbox);
+todolist.append(newTodo);
+input.value="";
 }
 form.addEventListener('submit',handleSubmit);
-// --------------OR-----------------
-/* form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    console.log('Submitted');
-}) */
+

72d09949e0b39b6114be49fc21792db61574502c
Aashish Panthee <aashishpanthee060@gmail.com>
2023-08-12 12:50:53 +0545

    Working with Events

--- miniproject/dist/index.js
+++ miniproject/dist/index.js
@@ -1,6 +1,14 @@
 "use strict";
-function printDouble(msg) {
-    console.log(msg);
+const btn = document.getElementById('btn');
+const input = document.getElementById('todoinput');
+const form = document.querySelector('#todoform');
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log('Submitted');
 }
-;
-printDouble("Hello Mr. Someone");
+form.addEventListener('submit', handleSubmit);
+// --------------OR-----------------
+/* form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    console.log('Submitted');
+}) */
--- miniproject/src/index.ts
+++ miniproject/src/index.ts
@@ -1,4 +1,14 @@
-function printDouble(msg:string){
-    console.log(msg);
-};
-printDouble("Hello Mr. Someone");
+const btn=document.getElementById('btn') as HTMLButtonElement;
+const input=document.getElementById('todoinput') as HTMLInputElement;
+const form=document.querySelector('#todoform') as HTMLFormElement
+function handleSubmit(e:SubmitEvent){
+e.preventDefault();
+console.log('Submitted');
+
+}
+form.addEventListener('submit',handleSubmit);
+// --------------OR-----------------
+/* form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    console.log('Submitted');
+}) */

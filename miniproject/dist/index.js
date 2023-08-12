2a5b5f02cface75dc798a5f0671f30d34563d67d
Aashish Panthee <aashishpanthee060@gmail.com>
2023-08-12 13:44:16 +0545

    Add Interface
    Todolist which was previously created was not that typescript friendly.
    Adding a Interface to the todolist will make it more typescript friendly.
    Same type of Object can be used in the todolist.

--- miniproject/dist/index.js
+++ miniproject/dist/index.js
@@ -1,17 +1,25 @@
 "use strict";
+const todos = [];
 const btn = document.getElementById('btn');
 const input = document.getElementById('todoinput');
 const form = document.querySelector('#todoform');
 const todolist = document.querySelector('#todolist');
 function handleSubmit(e) {
     e.preventDefault();
-    const todotext = input.value;
+    const newTodoo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodoo);
+    todos.push(newTodoo);
+    input.value = "";
+}
+function createTodo(todo) {
     const newTodo = document.createElement('li');
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    newTodo.append(todotext);
+    newTodo.append(todo.text);
     newTodo.append(checkbox);
     todolist.append(newTodo);
-    input.value = "";
 }
 form.addEventListener('submit', handleSubmit);
--- miniproject/src/index.ts
+++ miniproject/src/index.ts
@@ -1,17 +1,34 @@
+interface Todo{
+    text:string;
+    completed:boolean;
+}
+
+const todos:Todo[]=[];
+
 const btn=document.getElementById('btn') as HTMLButtonElement;
 const input=document.getElementById('todoinput') as HTMLInputElement;
 const form=document.querySelector('#todoform') as HTMLFormElement
 const todolist=document.querySelector('#todolist')!;
+
 function handleSubmit(e:SubmitEvent){
 e.preventDefault();
-const todotext=input.value;
+const newTodoo:Todo={
+    text:input.value,
+    completed:false,
+}
+createTodo(newTodoo)
+todos.push(newTodoo);
+input.value="";
+}
+
+function createTodo(todo:Todo){
 const newTodo=document.createElement('li');
 const checkbox=document.createElement('input');
 checkbox.type='checkbox';
-newTodo.append(todotext);
+newTodo.append(todo.text);
 newTodo.append(checkbox);
 todolist.append(newTodo);
-input.value="";
 }
+
 form.addEventListener('submit',handleSubmit);
 

db311ff152c9fe8f34910167f1281ad230362455
Aashish Panthee <aashishpanthee060@gmail.com>
2023-08-12 11:56:42 +0545

    Non-Null Assertion Operator
    The non-null assertion operator ( ! ) tells the compiler to assume that the value is not null or undefined and throws away any errors that come up from that assumption. This is similar to type assertion, but the difference is that you can use the non-null assertion operator on a nullable type, whereas you can only use type assertion on a type that is assignable to the type you are asserting it to.
    And You can also use Optional Chaining Operator ( ?. ) to avoid the error.

---
+++ miniproject/src/note.ts
@@ -0,0 +1,22 @@
+// Non-Null Assertion Operator 
+// !  Non-Null Assertion Operator
+// This operator is used to tell the compiler that the value is guaranteed to be not null;
+// NOTE : This operator is only used in TypeScript and not in JavaScript.
+// OR WE CAN USE OPTIONAL CHAINING OPERATOR (?.) INSTEAD OF NON-NULL ASSERTION OPERATOR (!)
+
+/* const btn=document.getElementById('btn')!
+console.log(btn);
+btn.addEventListener('click',()=>{
+    alert("Clicked !!")
+}) 
+This is using NON-NULL ASSERTION OPERATOR (!)
+*/
+
+
+/* const btn=document.getElementById('btn');
+console.log(btn);
+btn?Listener('click',()=>{
+    alert("Clicked !!")
+}) 
+This is using OPTIONAL CHAINING OPERATOR (!)
+*/

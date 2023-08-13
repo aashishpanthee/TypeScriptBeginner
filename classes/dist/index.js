282d9b91906da4034a6427d191dc7907e6696761
Aashish Panthee <aashishpanthee060@gmail.com>
2023-08-13 09:42:26 +0545

    Getters and Setters
    Getter and Setter methods are used to get and set the properties of an object.
    Getter methods are called just like properties.
    Getter methods are used to get the properties of and object , we cannot set the properties using getter methods.
    Setter methods are used to set the properties of an object,
    To make getter and setter methods, we can use the get and set keywords.
    
    When we try to set the value using get method, it will throw error.
    For example:doggy.fullName="helo"; it will throw error.
    
    Setter methods always accepts one parameter, that is the value we are trying to set.

--- classes/dist/index.js
+++ classes/dist/index.js
@@ -3,6 +3,7 @@ class Player {
     constructor(first, last) {
         this.score = 0;
         this.age = 33;
+        this.secret = 444;
         this.first = first;
         this.last = last;
         this.secretMethod();
@@ -10,7 +11,16 @@ class Player {
     secretMethod() {
         console.log("Secret Method");
     }
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    set setSecretNumber(newNumb) {
+        this.secret = newNumb;
+    }
 }
 const doggy = new Player("CoCo", "Panthee");
 console.log(doggy);
 console.log(doggy.first + doggy.last);
+console.log(doggy.fullName);
+doggy.setSecretNumber = 44;
+console.log(doggy);
--- classes/src/index.ts
+++ classes/src/index.ts
@@ -3,6 +3,7 @@ class Player{
     public readonly last:string;
     public score:number=0;
     private age:number=33;
+    private secret:number=444;
     constructor(first:string,last:string){
         this.first=first;
         this.last=last;
@@ -11,7 +12,16 @@ class Player{
     private secretMethod():void{
         console.log("Secret Method")
     }
+    get fullName():string{
+        return `${this.first} ${this.last}`
+    }
+    set setSecretNumber(newNumb:number){
+        this.secret=newNumb;
+    }
 }
 const doggy=new Player("CoCo","Panthee");
 console.log(doggy);
 console.log(doggy.first+doggy.last);
+console.log(doggy.fullName)
+doggy.setSecretNumber=44;
+console.log(doggy);

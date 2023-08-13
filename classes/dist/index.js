39decaf2a228c5779d71f1e93623cf58a080c111
Aashish Panthee <aashishpanthee060@gmail.com>
2023-08-13 10:23:19 +0545

    Protected Modifier
    Protected Modifier is used in extended classes. It allows the child class to access the parent class properties and methods.
    If any properties are private defined in the parent class, then the child class cannot access them.
    So, if we declare those properties and methods as protected in parent class, then the child class can access them.

--- classes/dist/index.js
+++ classes/dist/index.js
@@ -4,6 +4,7 @@ class Player {
         this.score = 0;
         this.age = 33;
         this.secret = 444;
+        this.name = "Aashish";
         this.first = first;
         this.last = last;
         this.secretMethod();
@@ -24,3 +25,11 @@ console.log(doggy.first + doggy.last);
 console.log(doggy.fullName);
 doggy.setSecretNumber = 44;
 console.log(doggy);
+class newPlayer extends Player {
+    gettingName() {
+        return this.name;
+    }
+}
+const Nimo = new newPlayer("Ben", "Panthee");
+console.log(Nimo);
+console.log(Nimo.gettingName());
--- classes/src/index.ts
+++ classes/src/index.ts
@@ -4,6 +4,7 @@ class Player{
     public score:number=0;
     private age:number=33;
     private secret:number=444;
+    protected name:string="Aashish"
     constructor(first:string,last:string){
         this.first=first;
         this.last=last;
@@ -25,3 +26,11 @@ console.log(doggy.first+doggy.last);
 console.log(doggy.fullName)
 doggy.setSecretNumber=44;
 console.log(doggy);
+class newPlayer extends Player{
+    gettingName():string{
+        return this.name;
+    }
+}
+const Nimo=new newPlayer("Ben","Panthee");
+console.log(Nimo);
+console.log(Nimo.gettingName());

02c48b93ae79c0ae1f7428b2f1c0fef75de32d68
Aashish Panthee <aashishpanthee060@gmail.com>
2023-08-13 09:08:43 +0545

    Public Modifier and Reaonly Modifier
    Public Modifier is the default modifier in TypeScript. A public member can be accessed anywhere without any restrictions.
    Public variables or methods both can be accessed inside or outside the class.
    If we don't specify any access modifier, then by default it is public.
    It is written just to make the code more readable and readable to other developers.
    
    Readonly Modifier is used to make the variables as read-only. Read-only variables can be initialized at the time of declaration or within the constructor of the same class.
    It cannot be changed outside the class.
    An instance of class can access the readonly variable but cannot change it.

--- classes/src/index.ts
+++ classes/src/index.ts
@@ -1,6 +1,7 @@
 class Player{
-    first:string;
-    last:string;
+    public readonly first:string;
+    public readonly last:string;
+    public score:number=0;
     constructor(first:string,last:string){
         this.first=first;
         this.last=last;
